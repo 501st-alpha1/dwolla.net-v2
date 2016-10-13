c7b2f51d8f42d0079b44ed04a7eaee6b76ec3635
Scott Weldon <open-source@scott-weldon.com>
2016-10-13 10:11:32 -0700

    Add test for Customers.Create

---
+++ dwolla.net.test/CustomersTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Dwolla;
+using Dwolla.SerializableTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dwolla.net.test
+{
+  [TestClass]
+  public class CustomersTest
+  {
+    public Customers c = new Customers();
+
+    [TestMethod]
+    public void TestCreate()
+    {
+      var result = c.Create(new Dictionary<string, string>
+      {
+        {"firstName", "John"},
+        {"lastName", "Smith"},
+        {"email", "jsmith@example.com"},
+        {"ipAddress", "127.0.0.1"},
+      });
+      Assert.IsInstanceOfType(result, typeof(Customer));
+    }
+  }
+}
