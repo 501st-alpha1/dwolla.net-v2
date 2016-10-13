e4da2c70db88d133542d228c3693e2b41162fd5d
Scott Weldon <open-source@scott-weldon.com>
2016-10-13 09:43:04 -0700

    Add Customers endpoint with Create method

---
+++ Endpoints/Customers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Dwolla.SerializableTypes;
+
+namespace DwollaV2
+{
+  public class Customers : Rest
+  {
+    /// <summary>
+    ///   Creates a new Customer.
+    /// </summary>
+    /// <param name="aParams">Additional parameters</param>
+    /// <param name="altToken">Alternate OAuth token</param>
+    /// <returns>Customer object</returns>
+    public Customer Create(Dictionary<string, string> aParams = null, string altToken = null)
+    {
+      var data = new Dictionary<string, string>
+      {
+        {"oauth_token", altToken ?? C.dwolla_access_token},
+      };
+
+      if (aParams != null) data = aParams.Union(data).ToDictionary(k => k.Key, v => v.Value);
+      return DwollaParse<Customer>(Post("/customers", data));
+    }
+  }
+}
