d3216cfb506885647a1a5737539de4031f9b4ac9
Scott Weldon <open-source@scott-weldon.com>
2016-10-14 15:16:37 -0700

    Add support for _embedded.errors attribute

---
+++ Client/SerializableTypes/EmbeddedErrorList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Dwolla.SerializableTypes
+{
+  public class EmbeddedErrorList
+  {
+    public IList<EmbeddedErrorResponse> Errors { get; set; }
+  }
+}
