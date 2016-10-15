ae8f95f26c1da72531cd25fe8ac5ef648a7b2f08
Scott Weldon <open-source@scott-weldon.com>
2016-10-14 17:23:39 -0700

    Add Customer to SerializableTypes

---
+++ Client/SerializableTypes/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace DwollaV2.SerializableTypes
+{
+  public class Customer
+  {
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Type { get; set; }
+    public string Status { get; set; }
+    public string CreatedDate { get; set; }
+  }
+}
